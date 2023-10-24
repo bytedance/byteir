5d7147a5a2b2e3c100a5af33d7d2cdc6ac9360b7
wujiawei.aml <wujiawei.aml@bytedance.com>
2023-10-25 00:28:14 +0800

    typo fix

--- frontends/torch-frontend/torch-frontend/include/torch-frontend/Transforms/EliminateUselessOp.h
+++ frontends/torch-frontend/torch-frontend/include/torch-frontend/Transforms/EliminateUselessOp.h
@@ -1,4 +1,4 @@
-//===- EleminateUselessOp.h -----------------------------------*--- C++ -*-===//
+//===- EliminateUselessOp.h -----------------------------------*--- C++ -*-===//
 //
 // Copyright 2022 ByteDance Ltd. and/or its affiliates. All rights reserved.
 // Licensed under the Apache License, Version 2.0 (the "License");
--- frontends/torch-frontend/torch-frontend/lib/Transforms/EliminateUselessOp.cpp
+++ frontends/torch-frontend/torch-frontend/lib/Transforms/EliminateUselessOp.cpp
@@ -1,4 +1,4 @@
-//===- EleminateUselessOp.cpp -----------------------------------*- C++ -*-===//
+//===- EliminateUselessOp.cpp -----------------------------------*- C++ -*-===//
 //
 // Copyright 2022 ByteDance Ltd. and/or its affiliates. All rights reserved.
 // Licensed under the Apache License, Version 2.0 (the "License");
