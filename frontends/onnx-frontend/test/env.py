2364b710a456ef98e8f5ee85c82d425651132229
yan.xu0210 <yan.xu0210@bytedance.com>
2024-01-15 16:06:52 +0800

    [onnx-frontend] move large_model_path to test_large_model.py

--- frontends/onnx-frontend/test/env.py
+++ frontends/onnx-frontend/test/env.py
@@ -3,5 +3,3 @@ import os.path as osp
 import sys
 
 ONNX_FRONTEND_PATH = osp.join(os.environ["ONNX_FRONTEND_ROOT"], "build/onnx-frontend/src/onnx-frontend")
-ONNX_FRONTEND_OPT_PATH = osp.join(os.environ["ONNX_FRONTEND_ROOT"], "build/onnx-frontend/src/onnx-frontend-opt")
-LARGE_MODEL_PATH = os.environ["LARGE_MODEL_PATH"]
