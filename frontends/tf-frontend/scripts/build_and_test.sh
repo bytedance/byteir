159427d261f06417d1a519f061de82194b800f26
quanbo.liu <liuquanbo@bytedance.com>
2024-02-05 11:40:18 +0800

    [tf-frontend] print test failed message

--- frontends/tf-frontend/scripts/build_and_test.sh
+++ frontends/tf-frontend/scripts/build_and_test.sh
@@ -11,6 +11,6 @@ bash $PROJ_DIR/scripts/prepare.sh
 
 pushd $PROJ_DIR
 $PROJ_DIR/bazel --output_user_root=./build build //tools:tf-frontend //tools:tf-ext-opt
-$PROJ_DIR/bazel --output_user_root=./build test //tf_mlir_ext/tests:all --java_runtime_version=remotejdk_11
-$PROJ_DIR/bazel --output_user_root=./build test //tf_mlir_ext/numerical:all --java_runtime_version=remotejdk_11
+$PROJ_DIR/bazel --output_user_root=./build test --test_output=errors //tf_mlir_ext/tests:all --java_runtime_version=remotejdk_11
+$PROJ_DIR/bazel --output_user_root=./build test --test_output=errors //tf_mlir_ext/numerical:all --java_runtime_version=remotejdk_11
 popd
