6010f44a42ed9e349420d11ae7f6903de2741010
wujiawei.aml <wujiawei.aml@bytedance.com>
2024-02-20 21:48:51 +0800

    [chore] recover extra-library function signature

--- frontends/torch-frontend/torch-frontend/python/torch_frontend/compile.py
+++ frontends/torch-frontend/torch-frontend/python/torch_frontend/compile.py
@@ -28,9 +28,7 @@ _CUSTOM_OPS_IN_TORCH = [
 ]
 
 
-def byteir〇flash_attn_fwd〡shape(
-    q: List[int], k: List[int], v: List[int], dropout_p: float, softmax_scale: float, casual: bool, return_softmax: bool
-) -> Tuple[List[int], List[int], List[int], List[int], List[int], List[int], List[int], List[int]]:
+def byteir〇flash_attn_fwd〡shape(q: List[int], k: List[int], v: List[int], dropout_p: float, softmax_scale: float, casual: bool, return_softmax: bool) -> Tuple[List[int], List[int], List[int], List[int], List[int], List[int], List[int], List[int]]:
     batch_size = q[0]
     seqlen_q = q[1]
     num_heads = q[2]
@@ -41,15 +39,7 @@ def byteir〇flash_attn_fwd〡shape(
     return q, q, k, v, q, softmax_lse, softmax_return, rng_shape
 
 
-def byteir〇flash_attn_fwd〡dtype(
-    q_rank_dtype: Tuple[int, int],
-    k_rank_dtype: Tuple[int, int],
-    v_rank_dtype: Tuple[int, int],
-    dropout_p: float,
-    softmax_scale: float,
-    casual: bool,
-    return_softmax: bool,
-) -> Tuple[int, int, int, int, int, int, int, int]:
+def byteir〇flash_attn_fwd〡dtype(q_rank_dtype: Tuple[int, int], k_rank_dtype: Tuple[int, int], v_rank_dtype: Tuple[int, int], dropout_p: float, softmax_scale: float, casual: bool, return_softmax: bool) -> Tuple[int, int, int, int, int, int, int, int]:
     q_rank, q_dtype = q_rank_dtype
     return q_dtype, q_dtype, q_dtype, q_dtype, q_dtype, torch.float32, q_dtype, torch.int64
 
@@ -58,18 +48,7 @@ def byteir〇flash_attn_fwd〡has_value_semantics() -> None:
     return
 
 
-def byteir〇flash_attn_bwd〡shape(
-    dout: List[int],
-    q: List[int],
-    k: List[int],
-    v: List[int],
-    out: List[int],
-    softmax_lse: List[int],
-    dropout_p: float,
-    softmax_scale: float,
-    casual: bool,
-    rng: List[int],
-) -> Tuple[List[int], List[int], List[int], List[int], List[int]]:
+def byteir〇flash_attn_bwd〡shape(dout: List[int], q: List[int], k: List[int], v: List[int], out: List[int], softmax_lse: List[int], dropout_p: float, softmax_scale: float, casual: bool, rng: List[int]) -> Tuple[List[int], List[int], List[int], List[int], List[int]]:
     batch_size = q[0]
     seqlen_q = q[1]
     num_heads = q[2]
@@ -81,18 +60,7 @@ def byteir〇flash_attn_bwd〡shape(
     return q, k, v, d_softmax, dq_accum
 
 
-def byteir〇flash_attn_bwd〡dtype(
-    dout_rank_dtype: Tuple[int, int],
-    q_rank_dtype: Tuple[int, int],
-    k_rank_dtype: Tuple[int, int],
-    v_rank_dtype: Tuple[int, int],
-    out_rank_dtype: Tuple[int, int],
-    softmax_lse_rank_dtype: Tuple[int, int],
-    dropout_p: float,
-    softmax_scale: float,
-    casual: bool,
-    rng_rank_dtype: Tuple[int, int],
-) -> Tuple[int, int, int, int, int]:
+def byteir〇flash_attn_bwd〡dtype(dout_rank_dtype: Tuple[int, int], q_rank_dtype: Tuple[int, int], k_rank_dtype: Tuple[int, int], v_rank_dtype: Tuple[int, int], out_rank_dtype: Tuple[int, int], softmax_lse_rank_dtype: Tuple[int, int], dropout_p: float, softmax_scale: float, casual: bool, rng_rank_dtype: Tuple[int, int]) -> Tuple[int, int, int, int, int]:
     dq_rank, dq_dtype = q_rank_dtype
     dk_rank, dk_dtype = k_rank_dtype
     dv_rank, dv_dtype = v_rank_dtype
