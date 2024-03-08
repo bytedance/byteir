aa221a6ae984d8b4e6b34c33c5bf3f67460c1217
Zhekun(Josh) Zhang <32320144+zhekunz2@users.noreply.github.com>
2024-03-08 14:29:17 -0800

    [External Libs] Update flash attention kernels (#145)
    Update flash attention kernels to 2.5.6 + some fixes

--- scripts/clang_format_check.sh
+++ scripts/clang_format_check.sh
@@ -7,7 +7,7 @@ if [[ $git_status ]]; then
   exit 1
 fi
 
-find \( -name '*.cpp' -or -name '*.h' -or -name '*.cc' \) -not -path "./external/*" | xargs clang-format-13 -i -style=file
+find \( -name '*.cpp' -or -name '*.h' -or -name '*.cc' \) -not -path "./external/*" -not -path "./external_libs/*" | xargs clang-format-13 -i -style=file
 git_status=$(git status --porcelain)
 if [[ $git_status ]]; then
   echo "clang-format-13 is not happy, please run \"clang-format-13 -i -style=file /PATH/TO/foo.cpp\" to the following files"
