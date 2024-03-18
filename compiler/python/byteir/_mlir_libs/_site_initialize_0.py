a8e97297ccf4cbdae0505fd1dc7ddd03423d15c3
LiuYuanqiang <liuyuanqiang.yqliu@bytedance.com>
2024-03-18 14:54:11 +0800

    [compiler] add register_byre_dialect in python binding

--- compiler/python/byteir/_mlir_libs/_site_initialize_0.py
+++ compiler/python/byteir/_mlir_libs/_site_initialize_0.py
@@ -1,5 +1,5 @@
 def context_init_hook(context):
-    from ._byteir import register_cat_dialect, register_ace_dialect, register_ccl_dialect
+    from ._byteir import register_cat_dialect, register_ace_dialect, register_ccl_dialect, register_byre_dialect
     from ._byteir import register_dialect_extensions, register_translation_dialects
     from ._mlirHlo import register_mhlo_dialect
     from ._stablehlo import register_dialect as register_stablehlo_dialect
@@ -11,6 +11,7 @@ def context_init_hook(context):
     register_cat_dialect(context)
     register_ace_dialect(context)
     register_ccl_dialect(context)
+    register_byre_dialect(context)
     register_dialect_extensions(context)
     register_translation_dialects(context)
 
