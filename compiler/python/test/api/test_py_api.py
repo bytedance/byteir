56310fff564dbfafe7a709bb2b8f0981ad410520
LiuYuanqiang <liuyuanqiang.yqliu@bytedance.com>
2024-03-21 21:29:55 +0800

    add pytest

---
+++ compiler/python/test/api/test_py_api.py
@@ -0,0 +1,36 @@
+import os
+import tempfile
+import byteir
+from byteir import ir
+
+CUR_DIR = os.path.realpath(os.path.dirname(__file__))
+TEST_ROOT_DIR = CUR_DIR + "/../../../test/"
+
+temp_dir = tempfile.TemporaryDirectory()
+
+def test_compile_mlp_inference():
+    path = TEST_ROOT_DIR + "E2E/MLPInference/input.mlir"
+    byteir.compile(path, temp_dir.name + "/test.mlir", entry_func="forward")
+
+def test_translate_to_llvmbc():
+    path = TEST_ROOT_DIR + "Pipelines/Host/E2E/Case0/03b_ToLLVMIR.mlir"
+    context = ir.Context()
+    with open(path, "r") as f:
+        module = ir.Module.parse(f.read(), context)
+        assert byteir.translate_to_llvmbc(module, temp_dir.name + "/test.ll.bc")
+
+def test_serialize_byre():
+    path = TEST_ROOT_DIR + "Dialect/Byre/Serialization/Compatibility/version_1_0_0.mlir"
+    context = ir.Context()
+    with open(path, "r") as f:
+        module = ir.Module.parse(f.read(), context)
+        assert byteir.serialize_byre(module, "1.0.0", temp_dir.name + "/test.mlir.bc")
+
+def test_deserialize_byre():
+    paths = [TEST_ROOT_DIR + "Dialect/Byre/Serialization/Compatibility/version_1_0_0.mlir.bc",
+             TEST_ROOT_DIR + "Dialect/Byre/Serialization/Compatibility/version_1_0_0.mlir.bc.v0"]
+    context = ir.Context()
+    for path in paths:
+        with open(path, "rb") as f:
+            module = byteir.deserialize_byre(f.read(), context)
+            print(module.operation.get_asm())
