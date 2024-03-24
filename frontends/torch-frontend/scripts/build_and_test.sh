a3d9c0c61e67888f02ee9bc72d10b20b03cb3253
Yuanqiang Liu <liuyuanqiang.yqliu@bytedance.com>
2024-03-24 23:22:19 +0800

    [torch-frontend] refactor building method when involving torch-mlir (#160)
    * split `torch_mlir` from `torch_frontend`, change
    `torch_frontend.torch_mlir` to `torch_frontend;torch_mlir`

--- frontends/torch-frontend/scripts/build_and_test.sh
+++ frontends/torch-frontend/scripts/build_and_test.sh
@@ -28,6 +28,6 @@ cmake -S . \
 
 cmake --build ./build --target all
 
-PYTHONPATH=./build/python_packages/ python3 -m pytest torch-frontend/python/test
+PYTHONPATH=build/python_packages/:build/torch_mlir_build/python_packages/torch_mlir python3 -m pytest torch-frontend/python/test
 
 popd
