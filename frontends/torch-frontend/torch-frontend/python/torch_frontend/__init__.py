28c8d77a5e19b88dbd6eefdf1337625929934f06
LiuYuanqiang <liuyuanqiang.yqliu@bytedance.com>
2024-04-07 11:47:23 +0800

    [compiler] fix UAF on getBytecodeProducerString()

--- frontends/torch-frontend/torch-frontend/python/torch_frontend/__init__.py
+++ frontends/torch-frontend/torch-frontend/python/torch_frontend/__init__.py
@@ -1,5 +1,7 @@
 from ._mlir_libs._torchFrontend import *
 
+from .compile import DebugType
+
 from .fx_utils import list_decomposed_ops, preprocess_fx_graph, get_none_indices
 from .compile import convert_to_mhlo_via_torch_mlir, compile, compile_dynamo_model
 from .flash_attn_op import replace_flash_attn
