5f9927b432d963429736c26574ecb0dea26ac4c9
wujiawei.aml <wujiawei.aml@bytedance.com>
2024-05-31 00:24:51 +0800

    [torch-frontend] Add stabelhlo IR of whole Mixtral 8x7B model

--- frontends/torch-frontend/examples/inference/mixtral/infer_single_mixtral.py
+++ frontends/torch-frontend/examples/inference/mixtral/infer_single_mixtral.py
@@ -3,6 +3,7 @@ from typing import List
 
 import torch
 from torch._subclasses.fake_tensor import FakeTensorMode
+from torch.fx.experimental.symbolic_shapes import ShapeEnv
 from functorch.compile import aot_module, default_partition
 from torch.fx.experimental.proxy_tensor import maybe_disable_fake_tensor_mode
 from torch._decomp import (
@@ -18,53 +19,6 @@ from transformers.models.mixtral.modeling_mixtral import (
 )
 from transformers.models.mixtral.configuration_mixtral import MixtralConfig
 
-
-def fake_export_whole_mixtral():
-    model_conf = MixtralConfig()
-    with FakeTensorMode():
-        mixtral = MixtralModel(model_conf)
-        # step 1: fake init
-        print(mixtral)
-
-        """
-        MixtralModel(
-        (embed_tokens): Embedding(32000, 4096)
-        (layers): ModuleList(
-            (0-31): 32 x MixtralDecoderLayer(
-            (self_attn): MixtralSdpaAttention(
-                (q_proj): Linear(in_features=4096, out_features=4096, bias=False)
-                (k_proj): Linear(in_features=4096, out_features=1024, bias=False)
-                (v_proj): Linear(in_features=4096, out_features=1024, bias=False)
-                (o_proj): Linear(in_features=4096, out_features=4096, bias=False)
-                (rotary_emb): MixtralRotaryEmbedding()
-            )
-            (block_sparse_moe): MixtralSparseMoeBlock(
-                (gate): Linear(in_features=4096, out_features=8, bias=False)
-                (experts): ModuleList(
-                (0-7): 8 x MixtralBlockSparseTop2MLP(
-                    (w1): Linear(in_features=4096, out_features=14336, bias=False)
-                    (w2): Linear(in_features=14336, out_features=4096, bias=False)
-                    (w3): Linear(in_features=4096, out_features=14336, bias=False)
-                    (act_fn): SiLU()
-                )
-                )
-            )
-            (input_layernorm): MixtralRMSNorm()
-            (post_attention_layernorm): MixtralRMSNorm()
-            )
-        )
-        (norm): MixtralRMSNorm()
-        )
-        """
-
-        # step 2: torch.export
-        bsz = 5
-        seq_len = 7
-        vocab_size = 32000
-        token_ids = torch.randint(0, vocab_size, (bsz, seq_len))
-        exported_mixtral = torch.export.export(mixtral, (token_ids,))
-        print(exported_mixtral)
-
 def export_mixtral_decoding_layer():
     torch._dynamo.config.capture_dynamic_output_shape_ops = True
     model_conf = MixtralConfig(hidden_size=32)
@@ -79,5 +33,48 @@ def export_mixtral_decoding_layer():
     module = torch_frontend.compile_dynamo_model(mixtral_decoder_layer, output_type="stablehlo")
     print(module.operation.get_asm())
 
+
+def export_fake_whole_mixtral_model():
+    torch._dynamo.config.capture_dynamic_output_shape_ops = True
+    mixtral_config = MixtralConfig(attn_implementation="eager")
+    bsz = 3
+    seqlen = 1
+    past_kv_seqlen = 7
+    with FakeTensorMode(shape_env=ShapeEnv()):
+        mixtral_model = MixtralModel(mixtral_config)
+        input_ids = torch.randint(0, mixtral_config.vocab_size, (bsz, seqlen))
+
+        past_key_values = []
+        for i in range(mixtral_config.num_hidden_layers):
+            past_key_values.append(
+                (
+                    torch.rand(
+                        bsz,
+                        mixtral_config.num_key_value_heads,
+                        past_kv_seqlen,
+                        mixtral_config.hidden_size
+                        // mixtral_config.num_attention_heads,
+                    ),
+                    torch.rand(
+                        bsz,
+                        mixtral_config.num_key_value_heads,
+                        past_kv_seqlen,
+                        mixtral_config.hidden_size
+                        // mixtral_config.num_attention_heads,
+                    ),
+                )
+            )
+        exported_mixtral = torch.export.export(
+            mixtral_model,
+            (input_ids, None, None, past_key_values),
+        )
+        import torch_frontend
+        module = torch_frontend.compile_dynamo_model(
+            exported_mixtral,
+            output_type="stablehlo",
+        )
+        print(module.operation.get_asm())
+
 if __name__ == "__main__":
+    export_fake_whole_mixtral_model()
     export_mixtral_decoding_layer()
