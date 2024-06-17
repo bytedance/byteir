a2ff87ed22e781915f72b76e6fcd6cb94810e677
zhengxuegui <zhengxuegui.0@bytedance.com>
2024-06-17 20:15:44 +0800

    format

--- compiler/lib/Dialect/mhlo/Transforms/FuseTransposeIntoDotGeneral.cpp
+++ compiler/lib/Dialect/mhlo/Transforms/FuseTransposeIntoDotGeneral.cpp
@@ -1,5 +1,4 @@
-//===- FuseTransposeIntoDotGeneral.cpp ---------------------------------*--- C++
-//-*-===//
+//===- FuseTransposeIntoDotGeneral.cpp ------------------------*--- C++ -*-===//
 //
 // Copyright 2022 ByteDance Ltd. and/or its affiliates. All rights reserved.
 // Licensed under the Apache License, Version 2.0 (the "License");
