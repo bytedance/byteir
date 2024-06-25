3187ffaaa418de418c3d01a70b73ace140bedca8
zhengxuegui <zhengxuegui.0@bytedance.com>
2024-06-25 16:40:09 +0800

    add elementwise tiling pipeline

--- compiler/lib/Pipelines/GPU/ElementwiseCodegen.cpp
+++ compiler/lib/Pipelines/GPU/ElementwiseCodegen.cpp
@@ -19,14 +19,20 @@
 
 #include "byteir/Conversion/ToLLVM/ToLLVM.h"
 #include "byteir/Dialect/Linalg/TransformOps/LinalgExtTransformOps.h"
+#include "byteir/Dialect/SCF/Passes.h"
 #include "byteir/Dialect/Transform/IR/TransformExtOps.h"
 #include "byteir/Dialect/Transform/Transforms/TransformInsertion.h"
+#include "byteir/Dialect/mhlo/Transforms/HloFuser.h"
 #include "byteir/Pipelines/Common/Utils.h"
+#include "byteir/Transforms/AnchoredPipeline.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/GPU/IR/GPUDialect.h"
 #include "mlir/Dialect/Linalg/IR/Linalg.h"
 #include "mlir/Dialect/Linalg/TransformOps/LinalgTransformOps.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
 #include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "mlir/IR/BuiltinOps.h"
+#include "mlir/Transforms/Passes.h"
 #include "llvm/ADT/SmallSet.h"
 
 #include <optional>
@@ -163,6 +169,64 @@ bool isFusionTarget(linalg::GenericOp genericOp) {
   return true;
 }
 
+template <typename GPUAttrType>
+SmallVector<Attribute> getGPUMappingAttr(OpBuilder b, int64_t rank) {
+
+  SmallVector<gpu::MappingId> mapping;
+  mapping.reserve(rank);
+  int64_t dimMapping = static_cast<int64_t>(gpu::MappingId::LinearDim0);
+  for (int64_t i = 0; i < rank; ++i) {
+    mapping.emplace_back(static_cast<gpu::MappingId>(dimMapping++));
+  }
+
+  auto mappingAttrs = llvm::to_vector(
+      llvm::map_range(mapping, [&](gpu::MappingId dim) -> Attribute {
+        return GPUAttrType::get(b.getContext(), dim);
+      }));
+  return mappingAttrs;
+}
+
+struct MappingElementwiseToGPUPass
+    : PassWrapper<MappingElementwiseToGPUPass, OperationPass<func::FuncOp>> {
+  MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(MappingElementwiseToGPUPass)
+
+  void getDependentDialects(DialectRegistry &registry) const override {
+    registry.insert<gpu::GPUDialect>();
+  }
+
+  void runOnOperation() override {
+    func::FuncOp funcOp = getOperation();
+    scf::ForallOp topLevelForallOp;
+    OpBuilder b(funcOp);
+    funcOp->walk<WalkOrder::PreOrder>([&](scf::ForallOp forallOp) {
+      if (auto parentForallOp = forallOp->getParentOfType<scf::ForallOp>()) {
+        if (parentForallOp.getMapping().has_value()) {
+          auto parentMappingAttrs =
+              llvm::to_vector(parentForallOp.getMappingAttr());
+          bool hasBlockMapping =
+              llvm::any_of(parentMappingAttrs, [](Attribute attr) {
+                return isa<mlir::gpu::GPUBlockMappingAttr>(attr);
+              });
+          if (hasBlockMapping) {
+            int64_t rank = forallOp.getRank();
+            SmallVector<Attribute> curMapping =
+                getGPUMappingAttr<gpu::GPUThreadMappingAttr>(b, rank);
+            forallOp.setMappingAttr(b.getArrayAttr(curMapping));
+          }
+        }
+      } else {
+        // top level forall
+        if (!forallOp.getMapping().has_value()) {
+          int64_t rank = forallOp.getRank();
+          SmallVector<Attribute> curMapping =
+              getGPUMappingAttr<gpu::GPUBlockMappingAttr>(b, rank);
+          forallOp.setMappingAttr(b.getArrayAttr(curMapping));
+        }
+      }
+    });
+  }
+};
+
 void createGPUTileElementwiseTransformImpl(OpPassManager &pm,
                                            const std::string &anchor,
                                            const std::string &prefix,
@@ -198,6 +262,19 @@ void createGPUTileElementwiseTransformImpl(OpPassManager &pm,
 
   pm.addPass(createGenericTransformInsertionPass(config));
 }
+
+void createGPUTileElementwiseInSCFImpl(OpPassManager &pm,
+                                       int64_t maxBlockSize) {
+  auto elementwiseAnchor = getByteIRElementwiseFusionAttrName().str();
+  OpPassManager anchoredPM(func::FuncOp::getOperationName());
+  anchoredPM.addPass(createForallCollapsingPass());
+  anchoredPM.addPass(createForallTilingPass({maxBlockSize}));
+  anchoredPM.addPass(std::make_unique<MappingElementwiseToGPUPass>());
+  anchoredPM.addPass(createForallNormalizePass());
+  pm.addNestedPass<func::FuncOp>(
+      createAnchoredPipelinePass(elementwiseAnchor, anchoredPM));
+}
+
 } // namespace
 
 void mlir::createGPUTileElementwiseTransform(
@@ -205,4 +282,10 @@ void mlir::createGPUTileElementwiseTransform(
   invokeOpPassPipelineBuilder(createGPUTileElementwiseTransformImpl, pm,
                               options.funcAnchor, options.annotatePrefix,
                               options.warpSize, options.blockSize);
+}
+
+void mlir::createGPUTileElementwiseInSCF(
+    OpPassManager &pm, const GPUTileElementwiseInSCFOptions &options) {
+  invokeOpPassPipelineBuilder(createGPUTileElementwiseInSCFImpl, pm,
+                              options.maxBlockSize);
 }
