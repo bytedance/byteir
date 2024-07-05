5aeb24d5ecd4ceb7d538fcf9f7246dc84c984638
wujiawei.aml <wujiawei.aml@bytedance.com>
2024-07-05 17:57:49 +0800

    [tf-frontend] fix numpy version

--- frontends/tf-frontend/scripts/build_and_test.sh
+++ frontends/tf-frontend/scripts/build_and_test.sh
@@ -10,6 +10,7 @@ PROJ_DIR="$CUR_DIR/.."
 bash $PROJ_DIR/scripts/prepare.sh
 
 pushd $PROJ_DIR
+python3 -m pip install numpy==1.26.4 --force-reinstall
 python3 -m pip install /data00/mhlo_libraries/mhlo_tools-1.4.0-cp39-cp39-linux_x86_64.whl
 $PROJ_DIR/bazel --output_user_root=./build build --experimental_ui_max_stdouterr_bytes=-1 //tools:tf-frontend //tools:tf-ext-opt
 $PROJ_DIR/bazel --output_user_root=./build test --experimental_ui_max_stdouterr_bytes=-1 --test_output=errors //tf_mlir_ext/tests:all --java_runtime_version=remotejdk_11
