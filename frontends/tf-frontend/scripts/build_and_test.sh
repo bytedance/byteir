80160ed109a4fdad98e892028ae25687cbd62bcf
wujiawei.aml <wujiawei.aml@bytedance.com>
2024-07-05 17:26:25 +0800

    unlimit stdout/stderr size in bazel build

--- frontends/tf-frontend/scripts/build_and_test.sh
+++ frontends/tf-frontend/scripts/build_and_test.sh
@@ -11,7 +11,7 @@ bash $PROJ_DIR/scripts/prepare.sh
 
 pushd $PROJ_DIR
 python3 -m pip install /data00/mhlo_libraries/mhlo_tools-1.4.0-cp39-cp39-linux_x86_64.whl
-$PROJ_DIR/bazel --output_user_root=./build build //tools:tf-frontend //tools:tf-ext-opt
-$PROJ_DIR/bazel --output_user_root=./build test --test_output=errors //tf_mlir_ext/tests:all --java_runtime_version=remotejdk_11
-$PROJ_DIR/bazel --output_user_root=./build test --test_output=errors //tf_mlir_ext/numerical:all --java_runtime_version=remotejdk_11
+$PROJ_DIR/bazel --output_user_root=./build build --experimental_ui_max_stdouterr_bytes=-1 //tools:tf-frontend //tools:tf-ext-opt
+$PROJ_DIR/bazel --output_user_root=./build test --experimental_ui_max_stdouterr_bytes=-1 --test_output=errors //tf_mlir_ext/tests:all --java_runtime_version=remotejdk_11
+$PROJ_DIR/bazel --output_user_root=./build test --experimental_ui_max_stdouterr_bytes=-1 --test_output=errors //tf_mlir_ext/numerical:all --java_runtime_version=remotejdk_11
 popd
