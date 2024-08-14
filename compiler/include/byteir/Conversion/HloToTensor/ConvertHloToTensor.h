7706fb973a2e1052e6d191ec74bf1273912be249
shuhuiyao <shuhuiyao@bytedance.com>
2024-08-14 17:04:20 +0800

    update

--- compiler/include/byteir/Conversion/HloToTensor/ConvertHloToTensor.h
+++ compiler/include/byteir/Conversion/HloToTensor/ConvertHloToTensor.h
@@ -1,4 +1,4 @@
-//===- ConvertHloToTensor.h ---------------------------------------*--- C++-*-===//
+//===- ConvertHloToTensor.h ---------------------------------*---C++-*-===//
 //
 // Copyright 2022 ByteDance Ltd. and/or its affiliates. All rights reserved.
 // Licensed under the Apache License, Version 2.0 (the "License");
