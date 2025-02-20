e8b5ba589308a6fb4151c45f8f7c50861c34a765
Yuanqiang Liu <liuyuanqiang.yqliu@bytedance.com>
2025-02-20 16:26:01 +0800

    refactor test

--- frontends/torch-frontend/torch-frontend/python/test/test_utils/test_jit_transforms.py
+++ frontends/torch-frontend/torch-frontend/python/test/test_utils/test_jit_transforms.py
@@ -2,92 +2,14 @@ import torch
 import sys
 
 from torch_frontend.utils import replace_copy_fill_with_slice_scatter
-from torch_frontend import compile
 
 
-# fill_ related.
-class NSliceSliceFill(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    # (4,4,4)
-    def forward(self, x):
-        x1 = torch.ops.aten.slice(x, 1, 0, sys.maxsize, 1)
-        x2 = torch.ops.aten.slice(x1, 0, 0, sys.maxsize, 1)
-        x3 = torch.ops.aten.slice(x2, 2, 0, sys.maxsize, 1)
-        x4 = torch.ops.aten.slice(x3, 0, 0, 2, 1)
-        _ = torch.ops.aten.fill_(x4, 2.0)
-        x = x + 1
-        return x
-
-
-class NSliceSelectFill(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    # (4,4,4)
-    def forward(self, x):
-        x1 = torch.ops.aten.slice(x, 1, 0, sys.maxsize, 1)
-        x2 = torch.ops.aten.slice(x1, 0, 0, sys.maxsize, 1)
-        x3 = torch.ops.aten.slice(x2, 2, 0, sys.maxsize, 1)
-        x4 = torch.ops.aten.select(x3, 0, 2)
-        _ = torch.ops.aten.fill_(x4, 2.0)
-        x = x + 1
-        return x
-
-
-class SelectSliceFill(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    # (4, 4, 4)
-    def forward(self, x):
-        x1 = torch.select(x, 0, 1)
-        x2 = torch.ops.aten.slice(x1, 0, 0, sys.maxsize)
-        _ = torch.ops.aten.fill_(x2, -torch.inf)
-        x = x + 1
-        return x
-
-
-# copy_ related.
-class NSliceSliceCopy(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    # (4,4,4)
-    def forward(self, x):
-        x1 = torch.ops.aten.slice(x, 1, 0, sys.maxsize, 1)
-        x2 = torch.ops.aten.slice(x1, 0, 0, sys.maxsize, 1)
-        x3 = torch.ops.aten.slice(x2, 2, 0, sys.maxsize, 1)
-        x4 = torch.ops.aten.slice(x3, 0, 0, 2, 1)
-        zeros = torch.zeros((2, 4, 4))
-        _ = torch.ops.aten.copy_(x4, zeros)
-        x = x + 1
-        return x
-
-
-class NSliceSelectCopy(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    # (4,4,4)
-    def forward(self, x):
-        x1 = torch.ops.aten.slice(x, 1, 0, sys.maxsize, 1)
-        x2 = torch.ops.aten.slice(x1, 0, 0, sys.maxsize, 1)
-        x3 = torch.ops.aten.slice(x2, 2, 0, sys.maxsize, 1)
-        x4 = torch.ops.aten.select(x3, 0, 2)
-        zeros = torch.zeros((4, 4))
-        _ = torch.ops.aten.copy_(x4, zeros)
-        x = x + 1
-        return x
-
-
-def _test_model(model_class):
-    input = torch.randn(4, 4, 4)
+def _test_helper(model_class, inputs):
+    # input = torch.randn(4, 4, 4)
     model = model_class()
-    golden = model(input)
+    golden = model(*inputs)
 
-    ts_model = torch.jit.trace(model, (input,), check_trace=False)
+    ts_model = torch.jit.trace(model, inputs, check_trace=False)
     replace_copy_fill_with_slice_scatter(ts_model.graph)
     print(f"{model_class.__name__}: {ts_model.graph}")
 
@@ -110,16 +32,106 @@ def _test_model(model_class):
             has_slice_scatter = True
     assert has_slice_scatter
 
-    out = ts_model(input)
-    assert torch.allclose(golden, out)
+    out = ts_model(*inputs)
+    torch.testing.assert_close(golden, out)
+
+######################################################################
+# fill_ related
+
+class NSliceSliceFill(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    # (4, 4, 4)
+    def forward(self, x):
+        x1 = torch.ops.aten.slice(x, 1, 0, sys.maxsize, 1)
+        x2 = torch.ops.aten.slice(x1, 0, 0, sys.maxsize, 1)
+        x3 = torch.ops.aten.slice(x2, 2, 0, sys.maxsize, 1)
+        x4 = torch.ops.aten.slice(x3, 0, 0, 2, 1)
+        _ = torch.ops.aten.fill_(x4, 2.0)
+        x = x + 1
+        return x
+
+def test_nslice_slice_fill():
+    inputs = [torch.randn(4, 4, 4)]
+    _test_helper(NSliceSliceFill, inputs)
 
 
-def test_rewrite():
-    _test_model(NSliceSliceFill)
-    _test_model(NSliceSelectFill)
-    _test_model(SelectSliceFill)
-    _test_model(NSliceSliceCopy)
-    _test_model(NSliceSelectCopy)
+class NSliceSelectFill(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    # (4, 4, 4)
+    def forward(self, x):
+        x1 = torch.ops.aten.slice(x, 1, 0, sys.maxsize, 1)
+        x2 = torch.ops.aten.slice(x1, 0, 0, sys.maxsize, 1)
+        x3 = torch.ops.aten.slice(x2, 2, 0, sys.maxsize, 1)
+        x4 = torch.ops.aten.select(x3, 0, 2)
+        _ = torch.ops.aten.fill_(x4, 2.0)
+        x = x + 1
+        return x
+
+def test_nslice_select_fill():
+    inputs = [torch.randn(4, 4, 4)]
+    _test_helper(NSliceSelectFill, inputs)
 
 
-test_rewrite()
+class SelectSliceFill(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    # (4, 4, 4)
+    def forward(self, x):
+        x1 = torch.select(x, 0, 1)
+        x2 = torch.ops.aten.slice(x1, 0, 0, sys.maxsize)
+        _ = torch.ops.aten.fill_(x2, -torch.inf)
+        x = x + 1
+        return x
+
+def test_select_slice_fill():
+    inputs = [torch.randn(4, 4, 4)]
+    _test_helper(SelectSliceFill, inputs)
+
+
+######################################################################
+# copy_ related
+
+class NSliceSliceCopy(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    # (4, 4, 4)
+    def forward(self, x):
+        x1 = torch.ops.aten.slice(x, 1, 0, sys.maxsize, 1)
+        x2 = torch.ops.aten.slice(x1, 0, 0, sys.maxsize, 1)
+        x3 = torch.ops.aten.slice(x2, 2, 0, sys.maxsize, 1)
+        x4 = torch.ops.aten.slice(x3, 0, 0, 2, 1)
+        zeros = torch.zeros((2, 4, 4))
+        _ = torch.ops.aten.copy_(x4, zeros)
+        x = x + 1
+        return x
+
+def test_nslice_slice_copy():
+    inputs = [torch.randn(4, 4, 4)]
+    _test_helper(NSliceSliceCopy, inputs)
+
+
+class NSliceSelectCopy(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    # (4, 4, 4)
+    def forward(self, x):
+        x1 = torch.ops.aten.slice(x, 1, 0, sys.maxsize, 1)
+        x2 = torch.ops.aten.slice(x1, 0, 0, sys.maxsize, 1)
+        x3 = torch.ops.aten.slice(x2, 2, 0, sys.maxsize, 1)
+        x4 = torch.ops.aten.select(x3, 0, 2)
+        zeros = torch.zeros((4, 4))
+        _ = torch.ops.aten.copy_(x4, zeros)
+        x = x + 1
+        return x
+
+def test_nslice_select_copy():
+    inputs = [torch.randn(4, 4, 4)]
+    _test_helper(NSliceSelectCopy, inputs)
+
