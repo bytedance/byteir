e6ed72a88eed7f9a918dc72ca1bd551a07bcdfb2
liushanghao <somehow6@outlook.com>
2025-07-18 10:36:00 +0800

    [bug] TritonTemplate align to byteir

--- compiler/python/byteir/dialects/cat/ir_translator/backend/tit_registry.py
+++ compiler/python/byteir/dialects/cat/ir_translator/backend/tit_registry.py
@@ -37,3 +37,94 @@ def _dispatch_cat_gemm_rcr_bias_relu(op, inputs):
 def _dispatch_cat_gemm_rcr_bias(op, inputs):
     Y = tit_ops.Gemm(inputs=inputs, layout="rcr", is_bias=True)
     return [Y]
+
+@TRITONTemplateIRTranslator.register("cat.gemm_rcr_relu")
+def _dispatch_cat_gemm_rcr_relu(op, inputs):
+    Y = tit_ops.Gemm(inputs=inputs, layout="rcr", activation="relu")
+    return [Y]
+
+@TRITONTemplateIRTranslator.register("cat.gemm_rcr")
+def _dispatch_cat_gemm_rcr(op, inputs):
+    Y = tit_ops.Gemm(inputs=inputs, layout="rcr")
+    return [Y]
+
+@TRITONTemplateIRTranslator.register("cat.gemm_rrr_bias_relu")
+def _dispatch_cat_gemm_rrr_bias_relu(op, inputs):
+    Y = tit_ops.Gemm(inputs=inputs, layout="rrr", is_bias=True, activation="relu")
+    return [Y]
+
+@TRITONTemplateIRTranslator.register("cat.gemm_rrr_bias")
+def _dispatch_cat_gemm_rrr_bias(op, inputs):
+    Y = tit_ops.Gemm(inputs=inputs, layout="rrr", is_bias=True)
+    return [Y]
+
+@TRITONTemplateIRTranslator.register("cat.gemm_rrr_relu")
+def _dispatch_cat_gemm_rrr_relu(op, inputs):
+    Y = tit_ops.Gemm(inputs=inputs, layout="rrr", activation="relu")
+    return [Y]
+
+@TRITONTemplateIRTranslator.register("cat.gemm_rrr")
+def _dispatch_cat_gemm_rrr(op, inputs):
+    Y = tit_ops.Gemm(inputs=inputs, layout="rrr")
+    return [Y]
+
+@TRITONTemplateIRTranslator.register("cat.bmm_rrr")
+def _dispatch_cat_bmm_rrr(op, inputs):
+    Y = tit_ops.Bmm(inputs=inputs, layout="rrr")
+    return [Y]
+
+@TRITONTemplateIRTranslator.register("cat.bmm_rrr_add")
+def _dispatch_cat_bmm_rrr_add(op, inputs):
+    Y = tit_ops.Bmm(inputs=inputs, layout="rrr", is_bias=True)
+    return [Y]
+
+@TRITONTemplateIRTranslator.register("cat.bmm_rcr")
+def _dispatch_cat_bmm_rcr(op, inputs):
+    Y = tit_ops.Bmm(inputs=inputs, layout="rcr")
+    return [Y]
+
+@TRITONTemplateIRTranslator.register("cat.bmm_rcr_add")
+def _dispatch_cat_bmm_rcr_add(op, inputs):
+    Y = tit_ops.Bmm(inputs=inputs, layout="rcr", is_bias=True)
+    return [Y]
+
+@TRITONTemplateIRTranslator.register("cat.bmm_crr")
+def _dispatch_cat_bmm_crr(op, inputs):
+    Y = tit_ops.Bmm(inputs=inputs, layout="crr")
+    return [Y]
+
+@TRITONTemplateIRTranslator.register("cat.bmm_crr_add")
+def _dispatch_cat_bmm_crr_add(op, inputs):
+    Y = tit_ops.Bmm(inputs=inputs, layout="crr", is_bias=True)
+    return [Y]
+
+@TRITONTemplateIRTranslator.register("cat.bmm_ccr")
+def _dispatch_cat_bmm_ccr(op, inputs):
+    Y = tit_ops.Bmm(inputs=inputs, layout="ccr")
+    return [Y]
+
+@TRITONTemplateIRTranslator.register("cat.bmm_ccr_add")
+def _dispatch_cat_bmm_ccr_add(op, inputs):
+    Y = tit_ops.Bmm(inputs=inputs, layout="ccr", is_bias=True)
+    return [Y]
+
+@TRITONTemplateIRTranslator.register("cat.softmax")
+def _dispatch_cat_softmax(op, inputs):
+    dim = mlir_attr_to_pyobj(op.attributes["dim"])
+    Y = tit_ops.Softmax(inputs=inputs,dim=dim,enable_online=True)
+    return [Y]
+
+@TRITONTemplateIRTranslator.register("cat.layernorm")
+def _dispatch_cat_layernorm(op, inputs):
+    axises = mlir_attr_to_pyobj(op.attributes["axis"])
+    eps = mlir_attr_to_pyobj(op.attributes["epsilon"])
+    Y=tit_ops.Layernorm(inputs=inputs,axises=axises,eps=eps)
+    return [Y]
+
+@TRITONTemplateIRTranslator.register("mhlo.transpose")
+def _dispatch_mhlo_transpose(op, inputs):
+    dims = mlir_attr_to_pyobj(op.attributes["permutation"])
+    dims = dims.tolist()
+    dims_str = ''.join(map(str, dims))
+    Y=tit_ops.Transpose(inputs=inputs,permutation=dims_str)
+    return [Y]
